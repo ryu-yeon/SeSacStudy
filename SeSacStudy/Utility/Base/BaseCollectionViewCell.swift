165ec25b4088307355896bb5b42f0e188b66956f
ryu-yeon <rytak108@gmail.com>
2022-11-08 03:18:16 +0900

    BaseCollectionViewCell 생성

---
+++ SeSacStudy/Utility/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BaseCollectionViewCell.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/11/08.
+//
+
+import UIKit
+
+import SnapKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        
+    }
+    
+    func setConstraints() {
+    }
+}
