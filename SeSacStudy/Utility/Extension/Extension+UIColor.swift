6c486ffc460f0f200b3c8616f423447446622684
ryu-yeon <rytak108@gmail.com>
2022-11-08 03:17:55 +0900

    컬러값 세팅

---
+++ SeSacStudy/Utility/Extension/Extension+UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  Extension+UIColor.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/11/07.
+//
+
+import UIKit
+
+
+extension UIColor {
+    static let brandGreen = UIColor(red: 0.285, green: 0.863, blue: 0.574, alpha: 1)
+    static let whiteGreen = UIColor(red: 0.804, green: 0.958, blue: 0.881, alpha: 1)
+    static let yellowGreen = UIColor(red: 0.7, green: 0.921, blue: 0.381, alpha: 1)
+}
