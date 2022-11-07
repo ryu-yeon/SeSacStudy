83ee9e336ea31a0d16337a5020797b0832607b8c
ryu-yeon <rytak108@gmail.com>
2022-11-07 15:52:06 +0900

    SeSacStudy 생성

---
+++ SeSacStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
