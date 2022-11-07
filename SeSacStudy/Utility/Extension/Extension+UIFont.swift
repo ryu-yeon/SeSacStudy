0943b6348c100b17586c7fb96d27cf4630fa3b31
ryu-yeon <rytak108@gmail.com>
2022-11-08 03:37:18 +0900

    폰트값 세팅

---
+++ SeSacStudy/Utility/Extension/Extension+UIFont.swift
@@ -0,0 +1,17 @@
+//
+//  Extension+UIFont.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/11/07.
+//
+
+import UIKit
+
+extension UIFont {
+
+}
+    
+enum Font: String {
+    case NotoSansMedium = "NotoSansCJKKR-Medium"
+    case NotoSansRegular = "NotoSansCJKKR-Regylar"
+}
