21e9c435c9ffa3b4c7d13deecf7923478f37b2a4
ryu-yeon <rytak108@gmail.com>
2022-11-08 14:09:06 +0900

    Extension+UIColor 컬러 추가

--- SeSacStudy/Utility/Extension/Extension+UIColor.swift
+++ SeSacStudy/Utility/Extension/Extension+UIColor.swift
@@ -12,4 +12,16 @@ extension UIColor {
     static let brandGreen = UIColor(red: 0.285, green: 0.863, blue: 0.574, alpha: 1)
     static let whiteGreen = UIColor(red: 0.804, green: 0.958, blue: 0.881, alpha: 1)
     static let yellowGreen = UIColor(red: 0.7, green: 0.921, blue: 0.381, alpha: 1)
+    
+    static let gray1 = UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha: 1)
+    static let gray2 = UIColor(red: 0.937, green: 0.937, blue: 0.937, alpha: 1)
+    static let gray3 = UIColor(red: 0.887, green: 0.887, blue: 0.887, alpha: 1)
+    static let gray4 = UIColor(red: 0.821, green: 0.821, blue: 0.821, alpha: 1)
+    static let gray5 = UIColor(red: 0.742, green: 0.742, blue: 0.742, alpha: 1)
+    static let gray6 = UIColor(red: 0.667, green: 0.667, blue: 0.667, alpha: 1)
+    static let gray7 = UIColor(red: 0.533, green: 0.533, blue: 0.533, alpha: 1)
+    
+    static let successColor = UIColor(red: 0.384, green: 0.559, blue: 0.9, alpha: 1)
+    static let errorColor = UIColor(red: 0.913, green: 0.401, blue: 0.419, alpha: 1)
+    static let focus = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
 }
