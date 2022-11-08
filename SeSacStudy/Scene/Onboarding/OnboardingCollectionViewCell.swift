f5f14f4703ff92bf3f9d5ae861b5b02396f85da5
ryu-yeon <rytak108@gmail.com>
2022-11-08 10:18:32 +0900

    UI 수정

--- SeSacStudy/Scene/Onboarding/OnboardingCollectionViewCell.swift
+++ SeSacStudy/Scene/Onboarding/OnboardingCollectionViewCell.swift
@@ -34,13 +34,13 @@ final class OnboardingCollectionViewCell: BaseCollectionViewCell {
     
     override func setConstraints() {
         textLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide).offset(72)
+            make.bottom.equalTo(onboardingImage.snp.top).offset(-56)
             make.leading.trailing.equalTo(self).inset(40)
             make.height.equalTo(76)
         }
         
         onboardingImage.snp.makeConstraints { make in
-            make.top.equalTo(textLabel.snp.bottom).offset(56)
+            make.bottom.equalTo(self).offset(-10)
             make.leading.trailing.equalTo(self).inset(8)
             make.height.equalTo(onboardingImage.snp.width)
         }
