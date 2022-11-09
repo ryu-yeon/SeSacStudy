48abecae9c5b5b03cf0b76a633a5ef60230d6c3f
ryu-yeon <rytak108@gmail.com>
2022-11-10 08:20:49 +0900

    불필요한 변수 삭제

--- SeSacStudy/Scene/Onboarding/OnboardingViewModel.swift
+++ SeSacStudy/Scene/Onboarding/OnboardingViewModel.swift
@@ -14,8 +14,6 @@ final class OnboardingViewModel {
     
     let list = PublishSubject<[OnboardingData]>()
     
-    var index: Int?
-    
     let data: [OnboardingData] = [
         OnboardingData(text: "위치 기반으로 빠르게\n주위 친구를 확인", image: "onboarding_img1", range: 5),
         OnboardingData(text: "스터디를 원하는 친구를\n찾을 수 있어요", image: "onboarding_img2", range: 11),
@@ -25,6 +23,4 @@ final class OnboardingViewModel {
     func fetch() {
         list.onNext(data)
     }
-    
-
 }
