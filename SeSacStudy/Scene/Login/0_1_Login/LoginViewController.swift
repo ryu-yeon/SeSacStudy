20354af5ea93917a851b958f6506c17d4a7dbd57
ryu-yeon <rytak108@gmail.com>
2022-11-10 08:33:01 +0900

    명세서에 맞게 로직 수정

--- SeSacStudy/Scene/Login/0_1_Login/LoginViewController.swift
+++ SeSacStudy/Scene/Login/0_1_Login/LoginViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 import RxCocoa
 import RxSwift
+import Toast
 
 final class LoginViewController: BaseViewController {
     
@@ -20,26 +21,37 @@ final class LoginViewController: BaseViewController {
         self.view = mainView
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setNavigationBar()
         
-        mainView.numberTextField.becomeFirstResponder()       
+        mainView.numberTextField.becomeFirstResponder()
         
         mainView.numberTextField.rx.text
             .orEmpty
             .withUnretained(self)
             .bind { (vc, value) in
                 vc.mainView.lineView.backgroundColor = value != "" ? .black : .gray3
-                vc.mainView.numberTextField.text = vc.viewModel.withHypen(number: value)
-                let number = vc.mainView.numberTextField.text
-                vc.viewModel.isValid = vc.viewModel.isValidPhone(number: number!)
-                if vc.viewModel.isValid {
-                    vc.mainView.checkButton.backgroundColor = .brandGreen
-                } else {
-                    vc.mainView.checkButton.backgroundColor = .gray6
-                }
+                vc.viewModel.withHypen(number: value)
+                vc.mainView.numberTextField.text = vc.viewModel.phoneNumber
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.valid
+            .withUnretained(self)
+            .bind { (vc, value) in
+                vc.mainView.checkButton.backgroundColor = value ? .brandGreen : .gray6
             }
             .disposed(by: disposeBag)
         
@@ -47,15 +59,22 @@ final class LoginViewController: BaseViewController {
             .withUnretained(self)
             .bind { (vc, _) in
                 if vc.viewModel.isValid {
-                    print("인증시작")
-                    vc.viewModel.phoneNumber = vc.mainView.numberTextField.text ?? ""
-                    vc.viewModel.sendSMS()
-                    let nextVC = LoginCheckViewController()
-                    vc.navigationController?.pushViewController(nextVC, animated: true)
+                    let phoneNumber = vc.mainView.numberTextField.text ?? ""
+                    vc.viewModel.firebaseAuthManager.sendSMS(phoneNumber: phoneNumber) { error in
+                        if error != nil {
+                            vc.mainView.makeToast("에러가 발생했습니다. 다시 시도해주세요", duration: 1.0, position: .top)
+                        } else {
+                            let nextVC = LoginCheckViewController()
+                            vc.navigationController?.pushViewController(nextVC, animated: true)
+                            nextVC.viewModel.phoneNumber = phoneNumber
+                        }
+                    }
                 } else {
-                    print("잘못된 요청")
+                    vc.mainView.makeToast("잘못된 전화번호 형식입니다.", duration: 1.0, position: .top)
                 }
             }
             .disposed(by: disposeBag)
+        
+        viewModel.fetch()
     }
 }
--- SeSacStudy/Scene/Login/0_1_Login/LoginViewModel.swift
+++ SeSacStudy/Scene/Login/0_1_Login/LoginViewModel.swift
@@ -7,41 +7,36 @@
 
 import Foundation
 
-import FirebaseAuth
 import RxCocoa
 import RxSwift
 
 final class LoginViewModel {
     
-    var isValid = false
+    let firebaseAuthManager = FirebaseAuthManager()
     
+    var valid = PublishSubject<Bool>()
+    var isValid = false
     var phoneNumber = ""
     
-    func isValidPhone(number: String) -> Bool {
-        let phoneRegex = "^01([0-9])-?([0-9]{3,4})-?([0-9]{4})$"
-        let pred = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
-        return pred.evaluate(with: number)
-    }
-    
-    func withHypen(number: String) -> String {
+    func withHypen(number: String) {
         var stringWithHypen: String = number
         stringWithHypen = stringWithHypen.replacingOccurrences(of: "-", with: "")
         if stringWithHypen.count == 10  || stringWithHypen.count == 11 {
             stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
             stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.endIndex, offsetBy: -4))
         }
-        return stringWithHypen
+        phoneNumber = stringWithHypen
+        isValidPhone(phoneNumber: phoneNumber)
     }
     
-    func sendSMS() {
-        PhoneAuthProvider.provider()
-          .verifyPhoneNumber("+82 \(phoneNumber)", uiDelegate: nil) { verificationID, error in
-              if error != nil {
-                  print("인증 ERROR")
-                return
-              }
-              Auth.auth().languageCode = "kr"
-              UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
-          }
+    func isValidPhone(phoneNumber: String) {
+        let phoneRegex = "^01([0-9])-?([0-9]{3,4})-?([0-9]{4})$"
+        let pred = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+        isValid = pred.evaluate(with: phoneNumber)
+        fetch()
+    }
+    
+    func fetch() {
+        valid.onNext(isValid)
     }
 }
