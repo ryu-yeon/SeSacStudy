6c22541f0aad85ac234dad709006f7de63cf7e29
ryu-yeon <rytak108@gmail.com>
2022-11-11 20:03:17 +0900

    Profile(회원가입 정보) 생성

---
+++ SeSacStudy/Model/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/11/10.
+//
+
+import Foundation
+
+struct Profile {
+    var phoneNumber: String
+    var nickname: String
+    var birth: Date
+    var email: String
+    var gender: Int
+}
