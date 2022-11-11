6be98e6a2de2cb924db312b5707aba47ee30d2fe
ryu-yeon <rytak108@gmail.com>
2022-11-11 20:03:45 +0900

    User(사용자 정보) 생성

---
+++ SeSacStudy/Model/User.swift
@@ -0,0 +1,35 @@
+//
+//  User.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/11/10.
+//
+
+import Foundation
+
+struct User: Codable {
+    let uid: String
+    let phoneNumber: String
+    let email: String
+    let FCMtoken: String
+    let nick: String
+    let birth: String
+    let gender: Int
+    let study: String
+    let comment: [String]
+    let reputation: [Int]
+    let sesac: Int
+    let sesacCollection: [Int]
+    let background: Int
+    let purchaseToken: [String]
+    let transactionId: [String]
+    let reviewedBefore: [String]
+    let reportedNum: Int
+    let reportedUser: [String]
+    let dodgepenalty: Int
+    let dodgeNum: Int
+    let ageMin: Int
+    let ageMax: Int
+    let searchable: Int
+    let createdAt: String
+}
