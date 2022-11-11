250a4282ed798e707b48c634d5db9ad0c63e72eb
ryu-yeon <rytak108@gmail.com>
2022-11-11 20:04:25 +0900

    baseURL 추가

---
+++ SeSacStudy/Network/EndPoint.swift
@@ -0,0 +1,13 @@
+//
+//  EndPoint.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/11/10.
+//
+
+import Foundation
+
+struct EndPoint {
+    
+    static let baseURL = "http://api.sesac.co.kr:1207"
+}
