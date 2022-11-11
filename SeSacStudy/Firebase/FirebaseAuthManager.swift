1743cef4013afae3ade95c2e19af5cd6204c6dba
ryu-yeon <rytak108@gmail.com>
2022-11-11 20:08:36 +0900

    getIdToken 추가 에러메세지, 에러코드 구조화

--- SeSacStudy/Firebase/FirebaseAuthManager.swift
+++ SeSacStudy/Firebase/FirebaseAuthManager.swift
@@ -9,19 +9,32 @@ import Foundation
 
 import FirebaseAuth
 
+enum FBAError: Int {
+    case manyTry = 17010
+}
+
+enum FBAMessage: String {
+    case start = "전화 번호 인증 시작"
+    case manyTry = "과도한 인증 시도가 있었습니다. 나중에 다시 시도해 주세요."
+    case error = "에러가 발생했습니다. 다시 시도해주세요"
+    case invaild = "잘못된 전화번호 형식입니다."
+    case fail = "전화 번호 인증 실패"
+}
+
 final class FirebaseAuthManager {
     
-    func sendSMS(phoneNumber: String, handler: @escaping (Error?) -> Void) {
+    func sendSMS(phoneNumber: String, handler: @escaping (Error?, Int?) -> Void) {
         PhoneAuthProvider.provider()
           .verifyPhoneNumber("+82 \(phoneNumber)", uiDelegate: nil) { verificationID, error in
-              if error != nil {
-                  print(error!.localizedDescription)
-                  handler(error)
+              if let error = error as NSError? {
+                  let errCode = AuthErrorCode(_nsError: error)
+                  print(error.localizedDescription)
+                  handler(error, errCode.errorCode)
                 return
               }
               Auth.auth().languageCode = "kr"
               UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
-              handler(error)
+              handler(error, 0)
           }
     }
     
@@ -31,9 +44,24 @@ final class FirebaseAuthManager {
         
         Auth.auth().signIn(with: credential) { authResult, error in
             print("SIGNIN")
-            print("CREDENTIAL",credential)
-            print("AUTHRESULT",authResult)
             handler(error)
+        }
+    }
+    
+    func getIdToken(complitionHandler: @escaping (String?) -> Void) {
+        guard let currentUser = Auth.auth().currentUser else {
+            complitionHandler(nil)
+            return
+        }
+        currentUser.getIDTokenForcingRefresh(true) { idToken, error in
+            if error != nil {
+                print("ID TOKEN ERROR")
+                complitionHandler(nil)
+            return
+          }
+            complitionHandler(idToken)
+            UserDefaults.standard.set(idToken, forKey: "token")
+            print("ID TOKEN: ", UserDefaults.standard.string(forKey: "token")!)
         }
     }
 }
