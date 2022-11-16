7d3e83ac6029c1c267719cf9acd636b51bf7b7b2
ryu-yeon <rytak108@gmail.com>
2022-11-16 23:09:49 +0900

    구조체 변수 수정

--- SeSacStudy/Model/User.swift
+++ SeSacStudy/Model/User.swift
@@ -11,25 +11,25 @@ struct User: Codable {
     let uid: String
     let phoneNumber: String
     let email: String
-    let FCMtoken: String
+    var FCMtoken: String
     let nick: String
     let birth: String
-    let gender: Int
-    let study: String
-    let comment: [String]
-    let reputation: [Int]
-    let sesac: Int
-    let sesacCollection: [Int]
-    let background: Int
-    let purchaseToken: [String]
-    let transactionId: [String]
-    let reviewedBefore: [String]
-    let reportedNum: Int
-    let reportedUser: [String]
-    let dodgepenalty: Int
-    let dodgeNum: Int
-    let ageMin: Int
-    let ageMax: Int
-    let searchable: Int
-    let createdAt: String
+    var gender: Int
+    var study: String
+    var comment: [String]
+    var reputation: [Int]
+    var sesac: Int
+    var sesacCollection: [Int]
+    var background: Int
+    var purchaseToken: [String]
+    var transactionId: [String]
+    var reviewedBefore: [String]
+    var reportedNum: Int
+    var reportedUser: [String]
+    var dodgepenalty: Int
+    var dodgeNum: Int
+    var ageMin: Int
+    var ageMax: Int
+    var searchable: Int
+    var createdAt: String
 }
