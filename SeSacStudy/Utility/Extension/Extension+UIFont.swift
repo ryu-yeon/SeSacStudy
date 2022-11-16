4cd5395181a5c962f97a871ca54825cd03ff3e66
ryu-yeon <rytak108@gmail.com>
2022-11-16 13:02:14 +0900

    오타 수정

--- SeSacStudy/Utility/Extension/Extension+UIFont.swift
+++ SeSacStudy/Utility/Extension/Extension+UIFont.swift
@@ -9,18 +9,18 @@ import UIKit
 
 extension UIFont {
     
-    static let display1_R20 = UIFont(name: "NotoSansCJKKR-Regylar", size: 20)
+    static let display1_R20 = UIFont(name: "NotoSansCJKKR-Regular", size: 20)
     static let display2_M24 = UIFont(name: "NotoSansCJKKR-Medium", size: 24)
     static let title1_M16 = UIFont(name: "NotoSansCJKKR-Medium", size: 16)
-    static let title2_R16 = UIFont(name: "NotoSansCJKKR-Regylar", size: 16)
+    static let title2_R16 = UIFont(name: "NotoSansCJKKR-Regular", size: 16)
     static let title3_M14 = UIFont(name: "NotoSansCJKKR-Medium", size: 14)
-    static let title4_R14 = UIFont(name: "NotoSansCJKKR-Regylar", size: 14)
+    static let title4_R14 = UIFont(name: "NotoSansCJKKR-Regular", size: 14)
     static let title5_M12 = UIFont(name: "NotoSansCJKKR-Medium", size: 12)
-    static let title6_R12 = UIFont(name: "NotoSansCJKKR-Regylar", size: 12)
+    static let title6_R12 = UIFont(name: "NotoSansCJKKR-Regular", size: 12)
     static let body1_M16 = UIFont(name: "NotoSansCJKKR-Medium", size: 16)
-    static let body2_R16 = UIFont(name: "NotoSansCJKKR-Regylar", size: 16)
-    static let body3_R14 = UIFont(name: "NotoSansCJKKR-Regylar", size: 14)
-    static let body4_R12 = UIFont(name: "NotoSansCJKKR-Regylar", size: 12)
-    static let caption_R10 = UIFont(name: "NotoSansCJKKR-Regylar", size: 10)
+    static let body2_R16 = UIFont(name: "NotoSansCJKKR-Regular", size: 16)
+    static let body3_R14 = UIFont(name: "NotoSansCJKKR-Regular", size: 14)
+    static let body4_R12 = UIFont(name: "NotoSansCJKKR-Regular", size: 12)
+    static let caption_R10 = UIFont(name: "NotoSansCJKKR-Regular", size: 10)
 
 }
