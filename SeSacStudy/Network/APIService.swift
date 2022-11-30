03fce7d02409d4b9052118419e38877db23bab04
ryu-yeon <rytak108@gmail.com>
2022-12-01 02:35:40 +0900

    search, stop, request, accept Study 서버 통신 추가

--- SeSacStudy/Network/APIService.swift
+++ SeSacStudy/Network/APIService.swift
@@ -136,4 +136,80 @@ final class APIService {
             }
         }
     }
+    
+    func searchStudy(idToken: String, lat: Double, long: Double, studyList: [String], complitionHandler: @escaping (Int) -> Void) {
+        let url = EndPoint.baseURL + "/v1/queue"
+        let headers: HTTPHeaders = [
+            "accept" : "application/json",
+            "idtoken": idToken
+        ]
+        
+        let parameters: [String: Any] = [
+            "long": long,
+            "lat": lat,
+            "studylist": studyList
+        ]
+        
+        let encoder = URLEncoding(arrayEncoding: .noBrackets)
+
+        AF.request(url, method: .post, parameters: parameters, encoding: encoder, headers: headers).responseString { response in
+            
+            let statusCode = response.response?.statusCode ?? 0
+
+            complitionHandler(statusCode)
+        }
+    }
+    
+    func stopSearchStudy(idToken: String, complitionHandler: @escaping (Int) -> Void) {
+        let url = EndPoint.baseURL + "/v1/queue"
+        let headers: HTTPHeaders = [
+            "accept" : "application/json",
+            "idtoken": idToken
+        ]
+
+        AF.request(url, method: .delete, headers: headers).responseString { response in
+            
+            let statusCode = response.response?.statusCode ?? 0
+
+            complitionHandler(statusCode)
+        }
+    }
+    
+    func requestStudy(idToken: String, uid: String, complitionHandler: @escaping (Int) -> Void) {
+        let url = EndPoint.baseURL + "/v1/queue/studyrequest"
+        let headers: HTTPHeaders = [
+            "accept" : "application/json",
+            "idtoken": idToken
+        ]
+        
+        let parameters: [String: String] = [
+            "otheruid": uid
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters, headers: headers).responseString { response in
+            
+            let statusCode = response.response?.statusCode ?? 0
+
+            complitionHandler(statusCode)
+        }
+    }
+    
+    func acceptStudy(idToken: String, uid: String, complitionHandler: @escaping (Int) -> Void) {
+        let url = EndPoint.baseURL + "/v1/queue/studyaccept"
+        let headers: HTTPHeaders = [
+            "accept" : "application/json",
+            "idtoken": idToken
+        ]
+        
+        let parameters: [String: String] = [
+            "otheruid": uid
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters, headers: headers).responseString { response in
+            
+            let statusCode = response.response?.statusCode ?? 0
+
+            complitionHandler(statusCode)
+        }
+    }
 }
