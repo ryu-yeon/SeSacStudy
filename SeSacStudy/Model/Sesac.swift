cd1e52c9f5b5683f11aa75349f818f1f0b6f2468
ryu-yeon <rytak108@gmail.com>
2022-12-01 02:36:10 +0900

    Hashable 수정

--- SeSacStudy/Model/Sesac.swift
+++ SeSacStudy/Model/Sesac.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Sesac: Codable {
+class Sesac: Codable, Hashable {
     let uid: String
     let nick: String
     let lat: Double
@@ -19,4 +19,26 @@ struct Sesac: Codable {
     let type: Int
     let sesac: Int
     let background: Int
+    
+    init(uid: String, nick: String, lat: Double, long: Double, reputation: [Int], studylist: [String], reviews: [String], gender: Int, type: Int, sesac: Int, background: Int) {
+        self.uid = uid
+        self.nick = nick
+        self.lat = lat
+        self.long = long
+        self.reputation = reputation
+        self.studylist = studylist
+        self.reviews = reviews
+        self.gender = gender
+        self.type = type
+        self.sesac = sesac
+        self.background = background
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uid)
+    }
+    
+    static func == (lhs: Sesac, rhs: Sesac) -> Bool {
+        lhs.uid == rhs.uid
+    }
 }
