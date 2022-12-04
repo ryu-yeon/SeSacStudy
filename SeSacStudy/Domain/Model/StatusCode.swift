284ffbd5cfa3655bd48b53d8760aafe8744fa0b6
ryu-yeon <rytak108@gmail.com>
2022-12-05 00:42:33 +0900

    StatusCode, MyQueueStatusCode 추가

--- SeSacStudy/Domain/Model/StatusCode.swift
+++ SeSacStudy/Domain/Model/StatusCode.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+enum StatusCode: Int {
+    case Success = 200
+    case FirebaseTokenError = 401
+    case NotSignupUser = 406
+    case ServerError = 500
+    case ClientError = 501
+    case UnknownError = -1
+}
+
 enum UserStatusCode: Int {
     case Success = 200
     case SignInUser = 201
@@ -21,5 +30,15 @@ enum UserStatusCode: Int {
 enum FirebaseAuthStatusCode: Int {
     case Success = 0
     case ManyTry = 17010
+    case UnknownError = -1
+}
+
+enum MyQueueStatusCode: Int {
+    case Success = 200
+    case NomalMode = 201
+    case FirebaseTokenError = 401
+    case NotSignupUser = 406
+    case ServerError = 500
+    case ClientError = 501
     case UnknownError = -1
 }
