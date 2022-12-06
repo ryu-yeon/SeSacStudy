8ddffc0313560f183f4a7d7c3f91e2b702216353
ryu-yeon <rytak108@gmail.com>
2022-12-07 07:09:34 +0900

    BackgroundImage 추가

---
+++ SeSacStudy/Domain/Model/Image.swift
@@ -0,0 +1,54 @@
+//
+//  Image.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/12/07.
+//
+
+import Foundation
+
+enum SesacImage: Int {
+    case sesac_face_1 = 0
+    case sesac_face_2
+    case sesac_face_3
+    case sesac_face_4
+    case sesac_face_5
+    
+    var image: String {
+        switch self {
+        case .sesac_face_1:
+            return "sesac_face_1"
+        case .sesac_face_2:
+            return "sesac_face_2"
+        case .sesac_face_3:
+            return "sesac_face_3"
+        case .sesac_face_4:
+            return "sesac_face_4"
+        case .sesac_face_5:
+            return "sesac_face_5"
+        }
+    }
+}
+
+enum BackgroundImage: Int {
+    case sesac_background_1 = 0
+    case sesac_background_2
+    case sesac_background_3
+    case sesac_background_4
+    case sesac_background_5
+    
+    var image: String {
+        switch self {
+        case .sesac_background_1:
+            return "sesac_background_1"
+        case .sesac_background_2:
+            return "sesac_background_2"
+        case .sesac_background_3:
+            return "sesac_background_3"
+        case .sesac_background_4:
+            return "sesac_background_4"
+        case .sesac_background_5:
+            return "sesac_background_5"
+        }
+    }
+}
