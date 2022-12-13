4b0970bdca6bcf0ca9bfc1a5b7d898b1bd2e67cd
ryu-yeon <rytak108@gmail.com>
2022-12-13 16:46:30 +0900

    RxKeyboard 추가 탭제스쳐 추가 Realm에 채팅기록 저장

--- SeSacStudy/Scene/Main/Home/Chat/View/ChatViewController.swift
+++ SeSacStudy/Scene/Main/Home/Chat/View/ChatViewController.swift
@@ -9,16 +9,13 @@ import UIKit
 
 import RxCocoa
 import RxSwift
+import RxKeyboard
 
 final class ChatViewController: BaseViewController {
     
     private let mainView = ChatView()
     let viewModel = ChatViewModel()
     private let disposeBag = DisposeBag()
-    
-    var dataSource: UICollectionViewDiffableDataSource<Int, Chat>!
-    
-    var snapshot = NSDiffableDataSourceSnapshot<Int, Chat>()
     
     override func loadView() {
         self.view = mainView
@@ -27,19 +24,14 @@ final class ChatViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        viewModel.loadChat()
+        viewModel.fetch()
         
-        viewModel.list
-            .withUnretained(self)
-            .bind { (vc, value) in
-                vc.mainView.tableView.reloadData()
-                vc.mainView.tableView.scrollToRow(at: IndexPath(row: (vc.viewModel.chatList?.payload.count ?? 1) - 1, section: 0), at: .bottom, animated: false)
-
-                vc.viewModel.socketManager.establishConnection()
-            }
-            .disposed(by: disposeBag)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(getMessage(notification:)), name: NSNotification.Name("getMessage"), object: nil)
+        viewModel.loadChat { [weak self] in
+            self?.setLabel()
+            self?.viewModel.fetch()
+            self?.reloadTableView()
+            self?.viewModel.socketManager.establishConnection()
+        }
         
         setRootButton()
         setNavigationBar()
@@ -47,6 +39,9 @@ final class ChatViewController: BaseViewController {
         setMenuButton()
         setTableView()
         setSendButton()
+        setKeyboard()
+        setTapGesture()
+        setChat()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,19 +54,26 @@ final class ChatViewController: BaseViewController {
         viewModel.socketManager.closeConnection()
     }
     
-    @objc func getMessage(notification: NSNotification) {
-            
-        let id = notification.userInfo!["id"] as! String
-        let chat = notification.userInfo!["chat"] as! String
-        let createdAt = notification.userInfo!["createdAt"] as! String
-        let from = notification.userInfo!["from"] as! String
-        let to = notification.userInfo!["to"] as! String
-        
-        let data = Chat(_id: id, to: to, from: from, chat: chat, createdAt: createdAt)
-        
-        viewModel.chatList?.payload.append(data)
-        mainView.tableView.reloadData()
-        mainView.tableView.scrollToRow(at: IndexPath(row: (viewModel.chatList?.payload.count ?? 1) - 1, section: 0), at: .bottom, animated: false)
+    private func setTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tap))
+        tap.cancelsTouchesInView = false
+        mainView.tableView.addGestureRecognizer(tap)
+    }
+    
+    @objc func tap() {
+        view.endEditing(true)
+    }
+    
+    private func setKeyboard() {
+        RxKeyboard.instance.visibleHeight
+                .drive(onNext: { [weak self] keyboardVisibleHeight in
+                    guard let self = self else { return }
+                    
+                    UIView.animate(withDuration: 0) {
+                        self.mainView.updateTextContainer(keyboardVisibleHeight: keyboardVisibleHeight)
+                        self.view.layoutIfNeeded()
+                    }
+                }).disposed(by: disposeBag)
     }
     
     private func setChatView() {
@@ -100,12 +102,17 @@ final class ChatViewController: BaseViewController {
             .disposed(by: disposeBag)
     }
     
+    private func setLabel() {
+        mainView.dateLabel.text = viewModel.calaculatedDate()
+        mainView.titleLabel.text = "\(viewModel.yourNickname)님과 매칭되었습니다."
+    }
+    
     private func checkStatusCode(_ statusCode: Int, chat: String, data: Chat?) {
         switch statusCode {
         case 200:
             if let data {
-                viewModel.chatList?.payload.append(data)
-                viewModel.list.onNext(viewModel.chatList!)
+                viewModel.chatRepository.saveChat(data: data)
+                reloadTableView()
             }
             print("채팅 전송 성공🟢")
         case 401:
@@ -150,32 +157,71 @@ final class ChatViewController: BaseViewController {
                 vc.present(nextVC, animated: true)
             }
             .disposed(by: disposeBag)
+        
+        mainView.reviewButton.rx.tap
+            .withUnretained(self)
+            .bind { (vc, _) in
+                nextVC.modalPresentationStyle = .overFullScreen
+                nextVC.setTitle(title: "리뷰 등록")
+                nextVC.setSubtitle(subtitle: "\(vc.viewModel.yourNickname)님과의 스터디는 어떠셨나요?")
+                nextVC.setSubtitleColor(.brandGreen)
+                nextVC.setOnlyOkButton()
+                nextVC.uid = vc.viewModel.yourID
+                nextVC.registerReview()
+                vc.present(nextVC, animated: true)
+            }
+            .disposed(by: disposeBag)
     }
     
     private func setTableView() {
-        
         mainView.tableView.delegate = self
         mainView.tableView.dataSource = self
-        
+    }
+    
+    private func setChat() {
+        NotificationCenter.default.addObserver(self, selector: #selector(getMessage(notification:)), name: NSNotification.Name("getMessage"), object: nil)
+    }
+    
+    @objc func getMessage(notification: NSNotification) {
+
+        let id = notification.userInfo!["id"] as! String
+        let chat = notification.userInfo!["chat"] as! String
+        let createdAt = notification.userInfo!["createdAt"] as! String
+        let from = notification.userInfo!["from"] as! String
+        let to = notification.userInfo!["to"] as! String
+
+        let data = Chat(_id: id, to: to, from: from, chat: chat, createdAt: createdAt)
+
+        viewModel.chatRepository.saveChat(data: data)
+        reloadTableView()
+    }
+    
+    private func reloadTableView() {
+        mainView.tableView.reloadData()
+        mainView.tableView.scrollToRow(at: IndexPath(row: viewModel.chatData.count - 1, section: 0), at: .bottom, animated: false)
     }
 }
 
 extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.chatList?.payload.count ?? 0
+        return viewModel.chatData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let chatList = viewModel.chatList?.payload else { return UITableViewCell() }
+        
+        guard let chatList = viewModel.chatData else { return UITableViewCell() }
+
         if chatList[indexPath.row].from == viewModel.myID {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: MyChatTableViewCell.reusableIdentifier, for: indexPath) as? MyChatTableViewCell else { return UITableViewCell()}
             
             cell.chatLabel.text = chatList[indexPath.row].chat
+            cell.dateLabel.text = viewModel.calculatedChatDate(creatAt: chatList[indexPath.row].createdAt)
             return cell
             
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: YourChatTableViewCell.reusableIdentifier, for: indexPath) as? YourChatTableViewCell else { return UITableViewCell()}
             cell.chatLabel.text = chatList[indexPath.row].chat
+            cell.dateLabel.text = viewModel.calculatedChatDate(creatAt: chatList[indexPath.row].createdAt)
             return cell
         }
     }
@@ -183,5 +229,4 @@ extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
-    
 }
---
+++ SeSacStudy/Scene/Main/Home/Chat/ViewModel/ChatViewModel.swift
@@ -0,0 +1,78 @@
+//
+//  ChatViewModel.swift
+//  SeSacStudy
+//
+//  Created by 유연탁 on 2022/12/01.
+//
+
+import Foundation
+
+import RealmSwift
+
+final class ChatViewModel {
+    
+    let chatService = ChatAPIService()
+    let dateFormatter = DateFormatter()
+    let chatRepository = ChatRepository()
+    
+    var chatData: Results<ChatRealm>!
+    let myID = UserDefaultsHelper.standard.loadUser()?.uid
+    var yourID = ""
+    var yourNickname = ""
+    var lastchatDate = "2000-01-01T00:00:00.000Z"
+    
+    lazy var socketManager = SocketIOMananger(myUID: UserDefaultsHelper.standard.loadUser()?.uid ?? "")
+
+    func fetch() {
+        chatData = chatRepository.fetchChat(yourID: yourID)
+        lastchatDate = chatData.last?.createdAt ?? "2000-01-01T00:00:00.000Z"
+    }
+    
+    func loadChat(complitionHandler: @escaping () -> Void) {
+        let idToken = UserDefaultsHelper.standard.idToken
+        chatService.loadChatList(idToken: idToken, from: yourID, lastchatDate: lastchatDate) { data, statusCode in
+            self.checkStatusCode(statusCode, data: data)
+            complitionHandler()
+        }
+    }
+    
+    private func checkStatusCode(_ statusCode: Int, data: ChatList?) {
+        switch statusCode {
+        case 200:
+            guard let data else { return }
+            data.payload.forEach {
+                chatRepository.saveChat(data: $0)
+            }
+            print("매칭 상태 확인 성공🟢")
+        case 401:
+            FirebaseTokenManager.shared.getIdToken { [weak self] idToken in
+                self?.chatService.loadChatList(idToken: idToken, from: self?.yourID ?? "", lastchatDate: self?.lastchatDate ?? "") { data, statusCode in
+                    self?.checkStatusCode(statusCode, data: data)
+                }
+            }
+            print("Firebase Token Error🔴")
+        case 406:
+            print("미가입 유저😀")
+        case 500:
+            print("Server Error🔴")
+        case 501:
+            print("Client Error🔴")
+        default:
+            break
+        }
+    }
+    
+    func calaculatedDate() -> String {
+        dateFormatter.dateFormat = "M월 d일 EEEE"
+        if let date = chatData.last?.createdAt {
+            return dateFormatter.string(from: (dateFormatter.date(from: date) ?? Date()))
+        } else {
+            return dateFormatter.string(from: Date())
+        }
+    }
+    
+    func calculatedChatDate(creatAt: String) -> String {
+        dateFormatter.dateFormat = "HH:mm"
+        return dateFormatter.string(from: (dateFormatter.date(from: creatAt) ?? Date()))
+    }
+}
